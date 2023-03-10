104c82a0df08bf634e8ad09c93139a7f4bcf519c
Administrator <administrator@Robss-MacBook-Pro.local>
2023-03-10 11:03:44 +0530

    added hello1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,7 +34,7 @@ class ArticlesController<ApplicationController
       if @article.user == current_user
         @article.update(article_params)
         @article.save
-        puts "hello"
+        puts "hello1"
         redirect_to @article
       else
         render 'edit'
